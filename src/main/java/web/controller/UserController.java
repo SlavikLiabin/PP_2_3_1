6964a0ce8997740b9bcbc12faf10bf66a68db511
Viacheslav Liabin <R9514633542@gmail.com>
2024-11-19 19:48:06 +0500

    fixed bugs to code

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -34,40 +34,18 @@ public class UserController {
         return "redirect:/";
     }
 
-//    @GetMapping("/deleteUser/{id}")
-//    public String deleteUser(@PathVariable Long id) {
-//        userService.deleteUser(id);
-//        return "redirect:/";
-//    }
-//
-//    @GetMapping("/updateUser/{id}")
-//    public String updateUser(@PathVariable Long id, Model model) {
-//        model.addAttribute("update", userService.getUserById(id));
-//        return "user-update";
-//    }
-//
-//    @PostMapping("/updateUser/{getId}")
-//    public String saveUpdateUser(@PathVariable Long getId, @ModelAttribute("user") User user) {
-//        user.setId(getId);
-//        userService.updateUser(user);
-//        return "redirect:/";
-//    }
-
-    // Заменяем @PathVariable на @RequestParam
     @GetMapping("/deleteUser")
     public String deleteUser(@RequestParam("id") Long id) {
         userService.deleteUser(id);
         return "redirect:/";
     }
 
-    // Заменяем @PathVariable на @RequestParam
     @GetMapping("/updateUser")
     public String updateUser(@RequestParam("id") Long id, Model model) {
         model.addAttribute("update", userService.getUserById(id));
         return "user-update";
     }
 
-    // Заменяем @PathVariable на @RequestParam
     @PostMapping("/updateUser")
     public String saveUpdateUser(@RequestParam("id") Long id, @ModelAttribute("user") User user) {
         user.setId(id);
